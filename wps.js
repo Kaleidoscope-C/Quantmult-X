04511b1b581903f96ed05d7e2a5a8c9b3ee93c21
Xchan984 <52686914+Xchan984@users.noreply.github.com>
2020-08-13 18:15:34 +0800

    Update wps.js

--- wps.js
+++ wps.js
@@ -6,32 +6,32 @@ obj= {
  "privilege": [
   {
    "times": 0,
-   "expire_time": 1896278400,
+   "expire_time": 4102415999,
    "spid": "data_recover"
   },
   {
    "times": 0,
-   "expire_time": 1896278400,
+   "expire_time": 4102415999,
    "spid": "ocr"
   },
   {
    "times": 0,
-   "expire_time": 1896278400,
+   "expire_time": 4102415999,
    "spid": "pdf2doc"
   },
   {
    "times": 0,
-   "expire_time": 1896278400,
+   "expire_time": 4102415999,
    "spid": "pdf_merge"
   },
   {
    "times": 0,
-   "expire_time": 1896278400,
+   "expire_time": 4102415999,
    "spid": "pdf_sign"
   },
   {
    "times": 0,
-   "expire_time": 1896278400,
+   "expire_time": 4102415999,
    "spid": "pdf_split"
   }
  ],
@@ -41,23 +41,23 @@ obj= {
  "exp": 0,
  "vip": {
   "memberid": 40,
-  "expire_time": 1896278400,
+  "expire_time": 4102415999,
   "name": "超级会员",
   "has_ad": 0,
   "enabled": [
    {
     "name": "超级会员",
-    "expire_time": 1896278400,
+    "expire_time": 4102415999,
     "memberid": 40
    },
    {
     "name": "WPS会员",
-    "expire_time": 1896278400,
+    "expire_time": 4102415999,
     "memberid": 20
    },
    {
     "name": "稻壳会员",
-    "expire_time": 1896278400,
+    "expire_time": 4102415999,
     "memberid": 12
    }
   ]
